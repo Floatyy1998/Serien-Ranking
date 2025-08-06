07c17ee7b349b53e53a9c50ecf07854c88b2dd77
Floatyy1998 <konrad.dinges@googlemail.com>
2025-08-06 18:35:44 +0200

    feat: Aktualisiere CACHE_VERSION auf v2025.08.06.1835 in Service Worker und Advanced Service Worker; verbessere Übergänge in ThreeDotMenu und SeriesCard

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,7 +4,7 @@
  */
 
 // Cache Versionen
-const CACHE_VERSION = 'v2025.08.06.1820';
+const CACHE_VERSION = 'v2025.08.06.1835';
 const STATIC_CACHE = `serien-static-${CACHE_VERSION}`;
 const DYNAMIC_CACHE = `serien-dynamic-${CACHE_VERSION}`;
 const API_CACHE = `serien-api-${CACHE_VERSION}`;
